8462b8cdf7583e5123d6f449c2d2db69d2b9cc62
masumkazibd <masum.kazi.hantmk@gmail.com>
2023-11-29 18:58:46 +0600

    list from database

--- Meeting_Management/Controllers/TaskController.cs
+++ Meeting_Management/Controllers/TaskController.cs
@@ -1,6 +1,7 @@
 ﻿using Meeting_Management.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,7 +13,23 @@ namespace Meeting_Management.Controllers
         MeetingInfoDbContext db = new MeetingInfoDbContext();
         public ActionResult Index()
         {
-            ViewBag.Names = db.Corporate_Custoer_Tbl.ToList();
+            //Customer Name
+            //if (personType == "Individual")
+            //{
+            //    ViewBag.CustomerNames = db.Individual_Customer_Tbl.Select(c => c.Individual_Customer_Name).ToList();
+            //}
+            //else if(personType == "Corporate")
+            //{
+            //    ViewBag.CustomerNames = db.Corporate_Custoer_Tbl.Select(c => c.Corporate_Custoer_Name).ToList();
+            //}
+            ViewBag.CustomerNames = db.Corporate_Custoer_Tbl.Select(c => c.Corporate_Custoer_Name).ToList();
+            //ViewBag.IndividualCustomerNames = db.Individual_Customer_Tbl.Select(c=>c.Individual_Customer_Name).ToList();
+
+            //ViewBag.CorporateCustomerNames = db.Corporate_Custoer_Tbl.Select(c=>c.Corporate_Custoer_Name).ToList();
+
+            //Service or Product
+            ViewBag.ProductNames = db.Products_Service_Tbl.Select(c=>c.Products_Service_Name).ToList();
+
             return View();
         }
     }
